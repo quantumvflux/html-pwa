f0f7b3aea8970d031636a72743aef8bcd48af0b7
Marcos <marcosabaravalle@gmail.com>
2024-04-19 13:30:18 -0300

    first commit

---
+++ public/service-worker.js
@@ -0,0 +1,18 @@
+// service-worker.js
+
+// Instalar el Service Worker
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("mi-pwa-cache").then(function (cache) {
+      return cache.addAll(["/", "/index.html", "/styles.css", "/script.js"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
